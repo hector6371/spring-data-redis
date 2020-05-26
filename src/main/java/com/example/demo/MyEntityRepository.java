0af7a00584507d2f6473dff0ab7c73a660146eb0
hector <hberlanga@gpsauriga.com>
2020-05-26 09:41:41 +0200

    Remove unused method

--- src/main/java/com/example/demo/MyEntityRepository.java
+++ src/main/java/com/example/demo/MyEntityRepository.java
@@ -3,11 +3,7 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface MyEntityRepository extends CrudRepository<MyEntity, Integer> {
-
-    Optional<MyEntity> findByIndex1(int index1);
 
 }
